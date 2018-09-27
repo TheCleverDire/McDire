72905691281f98070c639fb1b5bd001151cb014c
Paul Day <pday32@gmail.com>
2018-09-27 17:03:22 +0100

    Updating Software name and Internal version

--- MCGalaxy/Server/Server.Fields.cs
+++ MCGalaxy/Server/Server.Fields.cs
@@ -42,11 +42,11 @@ namespace MCGalaxy {
         public static PlayerMetaList Notes = new PlayerMetaList("text/notes.txt");
         
         /// <summary> *** DO NOT USE THIS! *** Use VersionString, as this field is a constant and is inlined if used. </summary>
-        public const string InternalVersion = "1.9.0.8";
+        public const string InternalVersion = "1.0.0.0";
         public static Version Version { get { return new Version(InternalVersion); } }
         public static string VersionString { get { return InternalVersion; } }
         
-        public static string SoftwareName = "MCGalaxy";
+        public static string SoftwareName = "MCDire";
         public static string SoftwareNameVersioned { get { return SoftwareName + " " + VersionString; } }
 
         // URL hash for connecting to the server
